a7a627a8623b50b0cbe21be291bde19b9d83e2ef
NatiaLobzhanidze <nati.lobzhanidze@gmail.com>
2022-08-18 10:52:58 +0400

    restart working on actor

--- NatiaL_HW23/ActorExtensions/FetchSimilarsFromID.swift
+++ NatiaL_HW23/ActorExtensions/FetchSimilarsFromID.swift
@@ -9,22 +9,21 @@ import Foundation
 extension CustomEvent {
     
     func getRendomElement (from arr: [TvShows]) {
-        semaphore.wait()
+       
         if let elem = arr.randomElement() {
             self.movieID = elem.id
             print(self.movieID)
-            semaphore.signal()
+           
         }
         
-        semaphore.wait()
-        semaphore.signal()
+     
     }
     
     // randomly choose tvShow and fetch similar tvShows
     
     func fetchSimilarTvShows() {
         
-        semaphore.wait()
+      
         api.getData(from: "https://api.themoviedb.org/3/tv/\(movieID)/similar?") { (result:Result<SimilarResponse, Error>) in
             
             switch result {
@@ -32,15 +31,14 @@ extension CustomEvent {
                 
                 self.similartvResult = success.results
                 print("Similar TvShows", success.results)
-                self.semaphore.signal()
+                
             case let .failure(failure):
                 
                 print("errors error",failure.localizedDescription)
             }
             
         }
-        semaphore.wait()
-        semaphore.signal()
+        
     }
     
 }
--- NatiaL_HW23/ActorExtensions/FetchTvShows.swift
+++ NatiaL_HW23/ActorExtensions/FetchTvShows.swift
@@ -9,22 +9,21 @@ import Foundation
 import UIKit
 extension CustomEvent {
     func fetchTvShows() {
-        semaphore.wait()
+     
         api.getData(from:"https://api.themoviedb.org/3/tv/top_rated?" ) { (result:Result<TvShowResponse, Error>) in
             
             switch result {
             case .success(let success):
                 self.tvResult = success.results
                 print("fetchTvResult",self.tvResult)
-                self.semaphore.signal()
+               
             case .failure(let failure):
                 print(failure.localizedDescription)
-                self.semaphore.signal()
+               
             }
             
         }
-        semaphore.wait()
-        semaphore.signal()
+      
         
     }
     
